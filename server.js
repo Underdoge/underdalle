d2e309d7171c5677c6c371c132c88c146c157207
Underdoge <echapa@live.com.mx>
2022-07-03 12:23:40 -0500

    serve the last file generated

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 'use strict';
 
 const
-    
+    fs = require('fs'),
     http = require('http'),
 
     app_port = process.env.app_port || 80,
     app_host = process.env.app_host || '127.0.0.1';
 
 http.createServer(function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
+    fs.readFile("dalle.jpg", function (err,data) {
+        if (err) {
+          res.writeHead(404);
+          res.end(JSON.stringify(err));
+          return;
+        }
+        res.writeHead(200);
+        res.end(data);
+      });
 }).listen(app_port);
 console.log(`Web server running at http://${app_host}:${app_port}`);
 
